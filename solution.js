fc81bb33226f77d6d2e50d168023bfc5815fd4a5
NgoziEmeh <35117703+NgoziEmeh@users.noreply.github.com>
2019-02-18 16:08:28 +0100

    Update solution.js

--- solution.js
+++ solution.js
@@ -23,6 +23,7 @@ if (johnAveScore > markAveScore){
     console.log('Mark\'s team is the winner with the highest average point of ' + markAveScore);
 };
 
+// 3. Change scores to show another as winner. Let me change John's team scores to 200, 120, 130 to make John's team to win.
 
 var johnTeamGame1 = 200;
 var johnTeamGame2 = 120;
@@ -33,7 +34,7 @@ var markTeamGame2 = 94;
 var markTeamGame3 = 123;
 
 
-// 3. change scores to show another as winner. Let me change John's team scores to 200, 120, 130 to make John's team to win.
+
 
 var johnAveScore = (johnTeamGame1 + johnTeamGame2 + johnTeamGame3)/3;
 console.log(johnAveScore);  //Average = 150
@@ -41,7 +42,9 @@ console.log(johnAveScore);  //Average = 150
 var markAveScore = (markTeamGame1 + markTeamGame2 + markTeamGame3)/3;
 console.log(markAveScore); //Average = 111
 
-Check the winner through the highest average score and console log it 
+
+Checking the winner through the highest average score and console logging it 
+
 if (johnAveScore > markAveScore){
     console.log('John\'s team is the winner with the highest average point of ' + johnAveScore);
 } else if (johnAveScore === markAveScore) {
